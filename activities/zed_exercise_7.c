9c94d3469087e993ace23437ed9bfe651d765cf0
Git-Mekbot <mekbotpowerup@gmail.com>
2020-03-19 09:43:14 -0600

    Finished exercise 8

--- activities/zed_exercise_7.c
+++ activities/zed_exercise_7.c
@@ -2,25 +2,32 @@
 
 int main(int argc, char *argv[])
 {
+// Declare an int and double for some fake bug data.
 	int bugs = 100;
-
 	double bug_rate = 1.2;
 	printf("You have %d bugs at the imaginary rate of %f.\n", bugs, bug_rate);
 
+// Declare a huge number using a type long for storing big numbers.
 	long universe_of_defects = 1L * 1024L * 1024L * 1024L;
-	printf("The entire universe has %ld bugs.\n", universe_of_defects);
 
+// Print out that number using %ld which adds a modifier to the usual %d.
+// Adding 'l' (the letter ell) means "print this as a long decimal".
+	printf("The entire universe has %ld bugs.\n", universe_of_defects);
 	double expected_bugs = bugs * bug_rate;
 	printf("You are expected to have %f bugs.\n", expected_bugs);
 
+// Bug rate compared to the bugs in the universe.
+// It's so small though that we have to use %e to print it in scientific notation.
 	double part_of_universe = expected_bugs / universe_of_defects;
 	printf("That is only a %e portion of the universe.\n", part_of_universe);
 
-	// this makes no sense, just a demo of something weird
+// Make a character, with a special syntax '\0' which creates a 'nul byte' character.
+// This is effectively the number 0.
 	char nul_byte = '\0';
 	int care_percentage = bugs * nul_byte;
-	printf("Which means you should care %d%%.\n", care_percentage);
 
+// Print and notice there are %% (two percent chars) so we can print a '%' (percent) character.
+	printf("Which means you should care %d%%.\n", care_percentage);
 	return 0;
 }
 
---
+++ activities/zed_exercise_8.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int areas[] = {10, 12, 13, 14, 20};
+
+// To C, name and full_name are identical methods of creating a char array.
+	char name[] = "Zed";
+	char full_name[] =
+	{
+		'Z', 'e', 'd', ' ', 'A', '.', ' ', 'S', 'h', 'a', 'w', '\0'
+	};
+
+// WARNING: On some systems you may have to change the
+// %ld in this code to a %u since it will use unsigned ints.
+// My computer thinks an int is 4 bytes in size. Might be different on 32bit or 64bit machines.
+	printf("The size of an int: %ld\n", sizeof(int));
+
+// The areas array has 5 integers in it, so it makes sense that the computer requires 20 bytes to store it.
+	printf("The size of areas (int[]): %ld\n", sizeof(areas));
+
+// If we divide the size of areas by size of an int then we get 5 elements.
+	printf("The number of ints in areas: %ld\n", sizeof(areas) / sizeof(int));
+
+//  C is "zero indexed" like Python and Ruby.
+	printf("The first area is %d, the 2nd %d.\n", areas[0],areas[1]);
+
+	printf("The size of a char: %ld\n", sizeof(char));
+
+// It says it's 4 bytes long, but we only typed "Zed" for 3 characters.
+	printf("The size of name (char[]): %ld\n", sizeof(name));
+	printf("The number of chars: %ld\n", sizeof(name) / sizeof(char));
+	printf("The size of full_name (char[]): %ld\n", sizeof(full_name));
+	printf("The number of chars: %ld\n", sizeof(full_name) / sizeof(char));
+
+// Print the name and full_name to prove that they actually are "strings" according to printf.
+	printf("name=\"%s\" and full_name=\"%s\"\n", name, full_name);
+	return 0;
+}
