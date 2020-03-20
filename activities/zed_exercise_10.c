4181fd222324ca511552ea3d01bdd7f69380dcd9
Git-Mekbot <mekbotpowerup@gmail.com>
2020-03-20 10:02:47 -0600

    finished 9 & 10 w/comments

--- activities/zed_exercise_10.c
+++ activities/zed_exercise_10.c
@@ -1,21 +1,27 @@
 #include <stdio.h>
 
+//  if you give one argument, the program's name is the first, so argc is 2.
 int main(int argc, char *argv[])
 {
 	int i = 0;
+
 // go through each string in argv
 // skipping argv[0]
+// tests that i is less than argc with the test i < argc . Since initially $1 < 2$ it will pass.
 	for(i = 1; i < argc; i++)
 	{
 		printf("arg %d: %s\n", i, argv[i]);
 	}
 
 // let's make our own array of strings
+// Same logic but used for states
 	char *states[] =
 	{
 		"California", "Oregon", "Washington", "Texas"
 	};
 	int num_states = 4;
+
+// Repeats until i is equal to or greater than the number of states.
 	for(i = 0; i < num_states; i++)
 	{
 		printf("state %d: %s\n", i, states[i]);
--- activities/zed_exercise_9.c
+++ activities/zed_exercise_9.c
@@ -4,10 +4,24 @@ int main(int argc, char *argv[])
 {
 	int numbers[4] = {0};
 	char name[4] = {'a'};
+
 	// first, print them out raw
+	// This is just trying to print them without setting them up beforehand.
+	// I didn't have to give all 4 elements of the arrays to initialize them.
+	// This is a short-cut that C has where, if you set just one element, it'll fill the rest in with 0.
 	printf("numbers: %d %d %d %d\n", numbers[0], numbers[1], numbers[2], numbers[3]);
+
+	// When each element of name is printed, only the first element 'a'
+	// shows up because the '\0' character is special and won't display.
+
 	printf("name each: %c %c %c %c\n", name[0], name[1], name[2], name[3]);
+
+	// The first time we print name it only prints "a" because,
+	// since the array will be filled with 0 after the first 'a' in the initializer,
+	// then the string is correctly terminated by a '\0' character
 	printf("name: %s\n", name);
+
+	// Now it gives them values
 	// setup the numbers
 	numbers[0] = 1;
 	numbers[1] = 2;
@@ -18,15 +32,24 @@ int main(int argc, char *argv[])
 	name[1] = 'e';
 	name[2] = 'd';
 	name[3] = '\0';
+
+	//And prints them out again
 	// then print them out initialized
 	printf("numbers: %d %d %d %d\n", numbers[0], numbers[1], numbers[2], numbers[3]);
 	printf("name each: %c %c %c %c\n", name[0], name[1], name[2], name[3]);
+
+	// Prints the whole name
 	// print the name like a string
 	printf("name: %s\n", name);
-	// another way to use name
+
+	// Same concept different execution
+	// Two syntaxes for doing a string: char name[4] = {'a'} and char *another = "name".
+	// The first one is less common and the second is what you should use for string literals like this.
 	char *another = "Zed";
 	printf("another: %s\n", another);
 	printf("another: %s\n", another);
 	printf("another each: %c %c %c %c\n", another[0], another[1], another[2], another[3]);
 	return 0;
+	// Note: printf thinks that the name is just a string. This is because to the C
+	// language there's no difference between a string and an array of characters.
 }
